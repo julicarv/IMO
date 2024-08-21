128eaeb392d02e92a42d14d85211e5721d6b0c11
julicarv <carvjulian3@gmail.com>
2024-08-21 14:26:18 -0400

    Created gdrmany

--- models.py
+++ models.py
@@ -2,18 +2,21 @@ import vars
 import numpy as np
 
 #Returns a matrix with the sizes of all of the people
-def gdr(params, scanDates, n=vars.n):
-    sizes = np.empty((n, len(scanDates[0])))
-    for i in range(n):
-        g = params[i, 0]
-        d = params[i, 1]
-        r = params[i, 2]
-        
-        for j in range(vars.numScans):
-            temp = max(vars.minSize, np.exp(g*scanDates[i, j] - d/r * (1 - np.exp(-r*scanDates[i, j]))))
-            if (temp > vars.maxSize):
-                sizes[i, j:vars.numScans] = vars.maxSize + 1
-                break
-            else:
-                sizes[i, j] = temp
+def gdr(params, scanDates):
+    sizes = np.empty((vars.numScans,))
+    g = params[0]
+    d = params[1]
+    r = params[2]
+    
+    for i in range(vars.numScans):
+        temp = max(vars.minSize, np.exp(g*scanDates[i] - d/r * (1 - np.exp(-r*scanDates[i]))))
+        if (temp > vars.maxSize):
+            sizes[i:vars.numScans] = vars.maxSize + 1
+            break
+        else:
+            sizes[i] = temp
+            
     return sizes
+
+def gdrMany(params, scanDates, n=vars.n):
+    return [gdr(params[i], scanDates[i]) for i in range(n)]
